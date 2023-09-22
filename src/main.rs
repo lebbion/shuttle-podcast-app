03d5597dd478b9824634199ca22d19de0c1d3649
lebbion <145786435+lebbion@users.noreply.github.com>
2023-09-22 18:30:56 +0300

    read podcasts from xml

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use std::io::BufReader;
+
+use anyhow::Result;
 use axum::{routing::get, Router};
+use xml::{reader::XmlEvent, EventReader};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
@@ -9,4 +13,78 @@ async fn axum() -> shuttle_axum::ShuttleAxum {
     let router = Router::new().route("/", get(hello_world));
 
     Ok(router.into())
+}
+
+struct Podcast {
+    title: String,
+    description: String,
+    audio_file: Option<String>,
+}
+
+impl Podcast {
+    fn new() -> Self {
+        Self {
+            title: String::new(),
+            description: String::new(),
+            audio_file: None,
+        }
+    }
+}
+
+enum ParseState {
+    Start,
+    InTitle,
+    InDescription,
+}
+
+async fn read_podcasts_from_xml(url: &str) -> Result<Vec<Podcast>> {
+    let mut results = Vec::new();
+
+    let data = reqwest::get(url).await?.text().await?;
+    let parser = EventReader::new(BufReader::new(data.as_bytes()));
+
+    let mut podcast = Podcast::new();
+    let mut state = ParseState::Start;
+
+    for event in parser {
+        match event {
+            Ok(XmlEvent::StartElement {
+                name, attributes, ..
+            }) => match name.local_name.as_str() {
+                "title" => state = ParseState::InTitle,
+                "description" => state = ParseState::InDescription,
+                "enclosure" => {
+                    podcast.audio_file = attributes.into_iter().find_map(|attr| {
+                        if attr.name.local_name == "url" {
+                            Some(attr.value)
+                        } else {
+                            None
+                        }
+                    });
+                }
+                _ => {}
+            },
+            Ok(XmlEvent::CData(content)) => match state {
+                ParseState::InTitle => {
+                    podcast.title = content;
+                    state = ParseState::Start;
+                }
+                ParseState::InDescription => {
+                    podcast.description = content;
+                    state = ParseState::Start;
+                }
+                _ => {}
+            },
+            Ok(XmlEvent::EndElement { name }) => {
+                if name.local_name == "item" {
+                    results.push(podcast);
+                    podcast = Podcast::new();
+                    state = ParseState::Start
+                }
+            }
+            _ => {}
+        }
+    }
+
+    Ok(results)
 }
